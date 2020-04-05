2f089bdd86f2237d357330872257ba996735d49b
QDesmettre <desmettre.quentin@gmail.com>
2020-04-05 20:41:43 +0200

    Login window files created

---
+++ Headers/login.h
@@ -0,0 +1,16 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QWidget>
+
+class LogIn : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit LogIn(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // LOGIN_H
---
+++ Sources/login.cpp
@@ -0,0 +1,6 @@
+#include "Headerslogin.h"
+
+LogIn::LogIn(QWidget *parent) : QWidget(parent)
+{
+
+}
