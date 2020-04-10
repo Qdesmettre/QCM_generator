3a1cc021725bac98bbdd00f4cc4c050beb913538
QDesmettre <desmettre.quentin@gmail.com>
2020-04-10 13:06:40 +0200

    attributes created

--- Headers/question.h
+++ Headers/question.h
@@ -1,11 +1,18 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 
+#include <vector>
+#include <QLabel>
+#include <QFormLayout>
 
 class Question
 {
 public:
     Question();
+private:
+    std::vector<QLabel*> m_choices;
+    QLabel *m_name;
+    QFormLayout *m_layout;
 };
 
 #endif // QUESTION_H
