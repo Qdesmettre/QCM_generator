ff0f901200d03dde117cd203b6329d30599b2d28
QDesmettre <desmettre.quentin@gmail.com>
2020-05-06 10:43:29 +0200

    Attributes aren't pointers anymore

--- Core/ProjectEdit/choice.h
+++ Core/ProjectEdit/choice.h
@@ -16,7 +16,6 @@ public:
     QString name() const;
     unsigned char num() const;
     bool isCorrect() const;
-    QHBoxLayout* layout();
 
     void setNum(unsigned char n);
 
@@ -27,11 +26,11 @@ signals:
     void destroyed(int const& index);
 
 private:
-    QPushButton *m_delete;
-    QLineEdit *m_name;
-    QLabel *m_num;
-    QCheckBox *m_correct;
-    QHBoxLayout *m_layout;
+    QPushButton m_delete;
+    QLineEdit m_name;
+    QLabel m_num;
+    QCheckBox m_correct;
+    QHBoxLayout m_layout;
 };
 
 #endif // CHOICE_H
