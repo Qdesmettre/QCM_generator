d888023c5e3fae29925cbe6107f56206f4eb3c37
QDesmettre <desmettre.quentin@gmail.com>
2020-05-06 10:36:50 +0200

    Revert "Choice's attributes switched from pointer to regular variables"
    This reverts commit b0903f922fadeddbb5a3d9cc61f2c1af273f7275.

--- Core/ProjectEdit/choice.cpp
+++ Core/ProjectEdit/choice.cpp
@@ -3,27 +3,38 @@
 Choice::Choice(const QString &name, const char &num, const bool &isCorrect, QWidget *parent):
     QWidget(parent)
 {
-    m_delete.setText("x");
-    m_name.setText(name);
-    m_num.setText(QString(char(96+num))+")");
-    m_correct.setChecked(isCorrect);
+    m_delete = new QPushButton("x");
+    m_name = new QLineEdit(name);
+    m_num = new QLabel(QString(char(96+num))+")");
+    m_correct = new QCheckBox;
+    m_correct->setChecked(isCorrect);
+    m_layout = new QHBoxLayout;
 
 
-    m_layout.addWidget(&m_num);
-    m_layout.addWidget(&m_name);
-    m_layout.addWidget(&m_correct);
-    m_layout.addWidget(&m_delete);
+    m_layout->addWidget(m_num);
+    m_layout->addWidget(m_name);
+    m_layout->addWidget(m_correct);
+    m_layout->addWidget(m_delete);
 
-    setLayout(&m_layout);
-    QObject::connect(&m_delete, SIGNAL(clicked()), this, SLOT(del()));
+
+
+    QObject::connect(m_delete, SIGNAL(clicked()), this, SLOT(del()));
 }
 void Choice::setNum(unsigned char n){
-    m_num.setText(QString(char(96+n))+")");
+    m_num->setText(QString(char(96+n))+")");
 }
 void Choice::del(){
-    emit destroyed(m_num.text().toStdString()[0]-97);
+    emit destroyed(m_num->text().toStdString()[0]-97);
     delete this;
 }
-QString Choice::name() const{return m_name.text();}
-unsigned char Choice::num() const{return m_num.text().remove(QString(")")).toUShort();}
-bool Choice::isCorrect() const{return m_correct.isChecked();}
+QString Choice::name() const{return m_name->text();}
+unsigned char Choice::num() const{return m_num->text().remove(QString(")")).toUShort();}
+bool Choice::isCorrect() const{return m_correct->isChecked();}
+QHBoxLayout* Choice::layout(){return m_layout;}
+Choice::~Choice(){
+    delete m_delete;
+    delete m_name;
+    delete m_num;
+    delete m_correct;
+    delete m_layout;
+}
--- Core/ProjectEdit/choice.h
+++ Core/ProjectEdit/choice.h
@@ -11,10 +11,12 @@ class Choice : public QWidget
     Q_OBJECT
 public:
     Choice(const QString &name = "", const char &num = 1, const bool &isCorrect = false, QWidget *parent = nullptr);
+    ~Choice();
 
     QString name() const;
     unsigned char num() const;
     bool isCorrect() const;
+    QHBoxLayout* layout();
 
     void setNum(unsigned char n);
 
@@ -25,11 +27,11 @@ signals:
     void destroyed(int const& index);
 
 private:
-    QPushButton m_delete;
-    QLineEdit m_name;
-    QLabel m_num;
-    QCheckBox m_correct;
-    QHBoxLayout m_layout;
+    QPushButton *m_delete;
+    QLineEdit *m_name;
+    QLabel *m_num;
+    QCheckBox *m_correct;
+    QHBoxLayout *m_layout;
 };
 
 #endif // CHOICE_H
